000eb7eafb29d5f3503ebac9fbbbb8de72119c58
Lukasz <lukasz.nowakiewicz@gmail.com>
2020-12-12 20:32:57 +0000

    Solution for this challenge.

--- main.py
+++ main.py
@@ -6,4 +6,20 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
+total1 = 0
+total2 = 0
 
+names = (name1+name2).lower()
+total1 = names.count("T".lower()) + names.count("R".lower()) + names.count("U".lower()) + names.count("E".lower())
+
+total2 = names.count("L".lower()) + names.count("O".lower()) + names.count("V".lower()) + names.count("E".lower())
+
+love_score = str(total1)+str(total2)
+total_love = int(love_score)
+
+if total_love < 10 or total_love > 90:
+  print(f"Your score is {total_love}, you go together like coke and mentos.")
+elif total_love >=40 and total_love <=50:
+  print(f"Your score is {total_love}, you are alright together.")
+else:
+  print(f"Your score is {total_love}.")
